b3d87d91bd30aed54173ee2f4541ee7eeceb8670
aparkin <aparkin>
2007-02-19 23:35:56 +0000

    Added the silly artwork.c example

---
+++ artwork.c
@@ -0,0 +1,38 @@
+int main (void)
+{
+
+	/* since a semicolon by itself is a valid statement, the lines below are
+	valid C code */
+
+	 ;;;;    ;;;;
+	;;;;;;  ;;;;;;
+	 ;;;;	 ;;;;
+
+	      ;;
+	     ;;;;
+
+	;;;	   ;;;
+	  ;;;    ;;;
+	    ;;;;;;
+
+
+
+     ;;;;      ;;;;
+    ;    ;    ;    ;
+   ;  {}  ;  ;  {}  ;
+    ;    ;    ;    ;
+     ;;;;      ;;;;
+
+           {}
+          ;{};
+         ;{{}};
+           ;;
+
+
+        ;;;;;;;;
+      ;; {}  {} ;;
+      ;;   {}   ;;
+        ;;;;;;;;
+
+	return 0;
+}
